7719c2c3e79449247aa2a16b0e9bf18ffa1f21c0
Gabriel <127769971+GabssssMes@users.noreply.github.com>
2025-09-01 15:52:58 +0200

    passwort geändert

--- Backend/pdfController.js
+++ Backend/pdfController.js
@@ -4883,7 +4883,7 @@ const sendMultipleMails = async (filesToSend, FullName, Steuer, Modul) => {
     secure: true,
     auth: {
       user: "messner92@gmail.com",
-      pass: "ddhskjinsnbfeeub",
+      pass: "ddhs kjin snbf eeub",
     },
   });
 
